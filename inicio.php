d2e048321ab28d45182ab790089f135f0e1e4df3
hugodose <hugo.dose@gmail.com>
2015-03-13 22:33:53 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -5,23 +5,25 @@
 echo 'Hello! ';
 
 require_once( 'src/Facebook/FacebookSession.php' );
+require_once( 'src/Facebook/FacebookRedirectLoginHelper.php' );
 
 echo 'Hello!2 ';
 use src\Facebook\FacebookSession;
 use src\Facebook\FacebookRequest;
 use src\Facebook\GraphUser;
 use src\Facebook\FacebookRequestException;
+use src\Facebook\FacebookRedirectLoginHelper;
 
 echo 'Hello!3 ';
 
 FacebookSession::setDefaultApplication('1560115454240194','e9b7a69cc961d012592996b2dd540e3a');
-
+echo 'Hello!4 ';
 
 $helper = new FacebookRedirectLoginHelper('http://globo.com');
 $loginUrl = $helper->getLoginUrl();
 // Use the login url on a link or button to redirect to Facebook for authentication
 
-echo 'Hello!4 ';
+echo 'Hello!5 ';
 
 
 ?>
