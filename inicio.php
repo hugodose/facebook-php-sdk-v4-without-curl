a46ac0f9769c2822e9dc671e3574025ea058b86d
hugodose <hugo.dose@gmail.com>
2015-03-13 21:31:21 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -4,33 +4,4 @@
 
 echo 'Hello!'
 
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-
-FacebookSession::setDefaultApplication('1560115454240194','e9b7a69cc961d012592996b2dd540e3a');
-
-// Use one of the helper classes to get a FacebookSession object.
-//   FacebookRedirectLoginHelper
-//   FacebookCanvasLoginHelper
-//   FacebookJavaScriptLoginHelper
-// or create a FacebookSession with a valid access token:
-$session = new FacebookSession('access-token-here');
-
-// Get the GraphUser object for the current user:
-
-try {
-  $me = (new FacebookRequest(
-    $session, 'GET', '/me'
-  ))->execute()->getGraphObject(GraphUser::className());
-  echo $me->getName();
-} catch (FacebookRequestException $e) {
-  // The Graph API returned an error
-} catch (\Exception $e) {
-  // Some other error occurred
-}
-
-
-
 ?>
