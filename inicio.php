3b4f8a12730770b60e2ddba786e12502727d10d1
hugodose <hugo.dose@gmail.com>
2015-03-13 23:20:34 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -4,19 +4,20 @@
 
 echo 'Hello! ';
 
-require_once( 'src/Facebook/FacebookSession.php' );
-require_once( 'src/Facebook/FacebookRedirectLoginHelper.php' );
+define('FACEBOOK_SDK_V4_SRC_DIR','/src/Facebook/');
 
-echo 'Hello!2 ';
+require_once("autoload.php");
 use src\Facebook\FacebookSession;
 use src\Facebook\FacebookRequest;
 use src\Facebook\GraphUser;
 use src\Facebook\FacebookRequestException;
 use src\Facebook\FacebookRedirectLoginHelper;
 
+echo 'Hello!2 ';
+
 echo 'Hello!3 ';
 
-//FacebookSession::setDefaultApplication('1560115454240194','e9b7a69cc961d012592996b2dd540e3a');
+FacebookSession::setDefaultApplication('1560115454240194','e9b7a69cc961d012592996b2dd540e3a');
 echo 'Hello!4 ';
 
 $helper = new FacebookRedirectLoginHelper('http://globo.com');
