4d04e2fe058dd6e35118c0616f1162c6e29ce37a
hugodose <hugo.dose@gmail.com>
2015-03-13 21:26:37 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -1,5 +1,32 @@
 <?php
-$helper = new FacebookRedirectLoginHelper('your redirect URL here');
-$loginUrl = $helper->getLoginUrl();
-// Use the login url on a link or button to redirect to Facebook for authentication
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+
+FacebookSession::setDefaultApplication('1560115454240194','e9b7a69cc961d012592996b2dd540e3a');
+
+// Use one of the helper classes to get a FacebookSession object.
+//   FacebookRedirectLoginHelper
+//   FacebookCanvasLoginHelper
+//   FacebookJavaScriptLoginHelper
+// or create a FacebookSession with a valid access token:
+$session = new FacebookSession('access-token-here');
+
+// Get the GraphUser object for the current user:
+
+try {
+  $me = (new FacebookRequest(
+    $session, 'GET', '/me'
+  ))->execute()->getGraphObject(GraphUser::className());
+  echo $me->getName();
+} catch (FacebookRequestException $e) {
+  // The Graph API returned an error
+} catch (\Exception $e) {
+  // Some other error occurred
+}
+
+
+
 ?>
