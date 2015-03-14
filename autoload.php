5ecd8e8da085099a35710aff1430f0a1cc4cac18
hugodose <hugo.dose@gmail.com>
2015-03-14 15:46:37 +0000

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -9,14 +9,4 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequestException.p
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookAuthorizationException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/GraphObject.php');
 
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
-use Facebook\FacebookSDKException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookAuthorizationException;
-use Facebook\GraphObject;
-use Facebook\GraphUser;
-
 ?>
