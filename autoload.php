ee6924da915cca63d7137df10fa191efa718f278
hugodose <hugo.dose@gmail.com>
2015-03-14 15:43:21 +0000

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,13 +1,22 @@
 <?php
-    session_start();
 
-    require_once( 'src/Facebook/FacebookSession.php' );
-    require_once( 'src/Facebook/FacebookRedirectLoginHelper.php' );
-    require_once( 'src/Facebook/FacebookRequest.php' );
-    require_once( 'src/Facebook/FacebookResponse.php' );
-    require_once( 'src/Facebook/FacebookSDKException.php' );
-    require_once( 'src/Facebook/FacebookRequestException.php' );
-    require_once( 'src/Facebook/FacebookAuthorizationException.php' );
-    require_once( 'src/Facebook/GraphObject.php' );
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSession.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRedirectLoginHelper.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequest.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookResponse.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSDKException.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequestException.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookAuthorizationException.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/GraphObject.php');
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
 
 ?>
