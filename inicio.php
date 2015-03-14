f93ef3c1f2d16d93b694112a86ae824aaa2fae6e
hugodose <hugo.dose@gmail.com>
2015-03-14 01:37:03 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -18,12 +18,13 @@ echo 'Hello! '.$_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSession.php';
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSession.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequest.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/GraphObject.php');
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSDKException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequestException.php');
 
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRedirectLoginHelper.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookResponse.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSDKException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookAuthorizationException.php');
 
 use Facebook\FacebookSession;
