2cd97f0971336b133131c8a8170b7003085d3af8
hugodose <hugo.dose@gmail.com>
2015-03-15 18:40:06 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -83,7 +83,7 @@ if ( isset( $session ) ) {
   
   //ATENCAO: invitable_friends or taggable_friends: the tokens returned through this API are not the same as the IDs returned via/me/friends.
   //friends: retorna o ID real, mas apenas de amigos que usam o aplicativo.
-  $taggable = (new FacebookRequest( $session, 'GET', '/me/taggable_friends' ))->execute()->getGraphObject()->asArray();
+  $taggable = (new FacebookRequest( $session, 'GET', '/me/invitable_friends' ))->execute()->getGraphObject()->asArray();
   //echo '<pre>' . print_r( $taggable, 1 ) . '</pre>';
   foreach ($taggable['data'] as $key => $value) {
      echo '<img class="friendthumb" src = "',$value->picture->data->url,'"/>';
