41fdeda40b38c281b32fb449d3a9258ff98935ed
hugodose <hugo.dose@gmail.com>
2015-03-15 05:48:24 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -85,7 +85,7 @@ if ( isset( $session ) ) {
   echo '<pre>' . print_r( $taggable, 1 ) . '</pre>';
   foreach($taggable['data'] as $indice) {
       echo "<div>" . $indice->name . " ... " . $indice->id . "</div>";
-      echo "<img src='" . $indice->picure->data->url . "'><br>";
+      echo "<img src='" . $indice->picure->data->url . "'></img><br>";
   }
   
   $friends = (new FacebookRequest($session, 'GET', '/me/friends'))->execute()->getGraphObject(GraphUser::className())->asArray();
