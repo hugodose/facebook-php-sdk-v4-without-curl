81c0b0991aac5589b5156abc91ad70bbfa0507e0
hugodose <hugo.dose@gmail.com>
2015-03-16 18:40:17 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -8,7 +8,7 @@
   $vardois = $_POST['content'];
   
   $sqltodo = "INSERT INTO person (FNAME, LNAME) VALUES ('um','dois')";
-  echo $sqltodo
+  echo $sqltodo;
   //funciona: $retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
   //funciona: $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
   echo '<br>'; 
