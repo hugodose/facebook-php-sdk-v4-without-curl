89bf9a665eaac59479fd0939d88ee3375e120436
hugodose <hugo.dose@gmail.com>
2015-03-16 11:34:53 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -1,10 +1,7 @@
 <html>
  <body>
-  <h2>Guestbook Entries</h2>
+  <h2>Entries</h2>
   <?php
-  //echo 'Hello, ' . htmlspecialchars($user->getNickname());
-
-  // Create a connection.
   $db = null;
   if (isset($_SERVER['SERVER_SOFTWARE']) &&
   strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
@@ -17,21 +14,11 @@
             )
         );
     }
-  } else {
-    // Connect from a development environment.
-    try{
-       $db = new pdo('mysql:host=127.0.0.1:3306;dbname=MinhaDB', 'root', '');
-    }catch(PDOException $ex){
-        die(json_encode(
-            array('outcome' => false, 'message' => 'Unable to connect')
-            )
-        );
-    }
   }
   try {
     // Show existing guestbook entries.
     foreach($db->query('SELECT * from person') as $row) {
-            echo "<div><strong>" . $row['FNAME'] . "</strong> wrote <br> " . $row['LNAME'] . "</div>";
+            echo "<div>" . $row['FNAME'] . " : " . $row['LNAME'] . "</div><br>";
      }
   } catch (PDOException $ex) {
     echo "An error occurred in reading or writing to guestbook.";
@@ -39,11 +26,11 @@
   $db = null;
   ?>
 
-  <h2>Sign the Guestbook</h2>
+  <h2>Input</h2>
   <form action="/sign" method="post">
     <div><textarea name="name" rows="3" cols="60"></textarea></div>
     <div><textarea name="content" rows="3" cols="60"></textarea></div>
-    <div><input type="submit" value="Sign Guestbook"></div>
+    <div><input type="submit" value="Input"></div>
   </form>
   </body>
 </html>
