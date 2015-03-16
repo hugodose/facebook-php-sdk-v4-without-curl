b276eb7f460fc63603d53ff6faf27b61fb7654e1
hugodose <hugo.dose@gmail.com>
2015-03-16 17:19:07 +0000

    Update consultadb.php

--- consultadb.php
+++ consultadb.php
@@ -1,76 +1,68 @@
  <?php
 class minhaclasse {
 
-private function criaDB(){
-$db = null;
-if (isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
-// Connect from App Engine.
-  try{
-    $db = new pdo('mysql:unix_socket=/cloudsql/hazel-proxy-88217:jogo;dbname=MinhaDB', 'root', '');
-    return $db;
-  }catch(PDOException $ex){
-    die(json_encode(
-        array('outcome' => false, 'message' => 'Unable to connect.')
-        )
-    );
+ private function criaDB(){
+  $db = null;
+  if (isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
+  // Connect from App Engine.
+    try{
+      $db = new pdo('mysql:unix_socket=/cloudsql/hazel-proxy-88217:jogo;dbname=MinhaDB', 'root', '');
+      return $db;
+    }catch(PDOException $ex){
+      die(json_encode(
+          array('outcome' => false, 'message' => 'Unable to connect.')
+          )
+      );
+    }
+  } else {
+    echo 'NOT connected from Google App Engine environment.';
   }
-} else {
-  echo 'NOT connected from Google App Engine environment.';
-}
-}
+ }
 
  
-function updateDB($SQLquery){
-$db = $this->criaDB();
-try {
-    //$sql = "UPDATE person SET FNAME='zzz91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1";
-    //$sql = "INSERT INTO person (FNAME, LNAME) VALUES ('opa','oma')";
-    
-    echo $SQLquery;
-    $sql = $SQLquery;
+ function updateDB($SQLquery){
+  $db = $this->criaDB();
+  try {
     // Prepare statement
-    $stmt = $db->prepare($sql);
-    //echo $stmt;
+    $stmt = $db->prepare($SQLquery);
     // execute the query
     $stmt->execute();
     // echo a message to say the UPDATE succeeded
-    echo $stmt->rowCount() . " rows successfully";
-} catch(PDOException $e) {
+    echo $SQLquery . ': ';
+    echo $stmt->rowCount() . " rows successfully <br>";
+  } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
-}
-$db = null;
-return $stmt;
-}
-
-
-function selectDB($SQLquery){
-$db = $this->criaDB();
-try {
-  echo $SQLquery;
-  $retorno = $db->query($SQLquery);
-  //$retorno->execute();
-} catch (PDOException $ex) {
-  echo "An error occurred in reading or writing to db.";
-}
-$db = null;
-return $retorno;
-}
-
-function consultaDB(){
-$db = $this->criaDB();
-
-try {
-  // Show existing guestbook entries.
-  // Show existing guestbook entries.
-  foreach($db->query('SELECT * from person LIMIT 1') as $row) {
-  echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . "</div>";
+    echo "An error occurred in reading or writing to db.";
   }
-} catch (PDOException $ex) {
-  echo "An error occurred in reading or writing to db.";
-}
-$db = null;
-return $row;
-}
+  $db = null;
+  return $stmt;
+ }
+
+
+//function selectDB($SQLquery){
+//$db = $this->criaDB();
+//try {
+//  echo $SQLquery;
+//  $retorno = $db->query($SQLquery);
+//} catch (PDOException $ex) {
+//  echo "An error occurred in reading or writing to db.";
+//}
+//$db = null;
+//return $retorno;
+//}
+
+//function consultaDB(){
+//$db = $this->criaDB();
+//try {
+//  foreach($db->query('SELECT * from person LIMIT 1') as $row) {
+//  echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . "</div>";
+//  }
+//} catch (PDOException $ex) {
+//  echo "An error occurred in reading or writing to db.";
+//}
+//$db = null;
+//return $row;
+//}
 
 }
 
