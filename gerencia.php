167c117aad1953f81d92c9f3bffb0c9fda06d430
hugodose <hugo.dose@gmail.com>
2015-03-16 14:22:53 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -3,33 +3,8 @@
   <h2>Entries</h2>
   <?php
   require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
-  
-  $db = null;
-  if (isset($_SERVER['SERVER_SOFTWARE']) &&
-  strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
-    // Connect from App Engine.
-    try{
-       $db = new pdo('mysql:unix_socket=/cloudsql/hazel-proxy-88217:jogo;dbname=MinhaDB', 'root', '');
-    }catch(PDOException $ex){
-        die(json_encode(
-            array('outcome' => false, 'message' => 'Unable to connect.')
-            )
-        );
-    }
-  }
-  try {
-    // Show existing guestbook entries.
-    foreach($db->query('SELECT * from person') as $row) {
-            echo "<div>" . $row['FNAME'] . " : " . $row['LNAME'] . "</div><br>";
-     }
-  } catch (PDOException $ex) {
-    echo "An error occurred in reading or writing to guestbook.";
-  }
-  $db = null;
-  
   $appObj = new minhaclasse();
   $appArray = $appObj->selectDB();
-  
   ?>
 
   <h2>Input</h2>
