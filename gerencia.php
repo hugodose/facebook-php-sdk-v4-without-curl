29f772f7fb5719c2fffe8edef459e70af6e801c9
hugodose <hugo.dose@gmail.com>
2015-03-16 17:26:40 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -5,7 +5,7 @@
   require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
  
   //$retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
-  $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
+  $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='e9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
   echo $retorno . '<br>'; 
   $retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('oi','oi2')" );
   echo $retorno . '<br>';
