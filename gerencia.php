f14070571dd22b0df56db8ba8a7e7e6aa45346a0
hugodose <hugo.dose@gmail.com>
2015-03-16 15:19:07 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -5,7 +5,10 @@
   require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
  
   //$retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
-  $retorno = (new minhaclasse())->selectDB('INSERT INTO person VALUES ("oi1", "oi2")' );
+  
+  $n1 = "oi1";
+  $n2 = "oi2";
+  $retorno = (new minhaclasse())->selectDB('INSERT INTO person VALUES (' . $n1 . ',' . $n2 . '')' );
   foreach($retorno as $row) {
   echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . "</div>";
   }
