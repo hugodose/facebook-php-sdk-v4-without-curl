ea69c1a92216316297fbbef1afe8080233a82808
hugodose <hugo.dose@gmail.com>
2015-03-16 16:43:30 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -4,16 +4,17 @@
   <?php
   require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
  
-  //$retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
+  $retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
   
   //$n1 = 'oi1';
   //$n2 = 'oi2';
+  $retorno = (new minhaclasse())->updateDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
   $retorno = (new minhaclasse())->updateDB("INSERT INTO person (FNAME, LNAME) VALUES ('oi','oi2')" );
 
  
   $retorno = (new minhaclasse())->selectDB('SELECT * from person');
   foreach($retorno as $row) {
-  echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . " " . $row['ID'] . "</div>";
+  echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . " " . $row['Id'] . "</div>";
   }
   
   $retorno = (new minhaclasse())->selectDB('SELECT * from person LIMIT 1');
