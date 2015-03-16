bcfddcd5f498681dbb5c93d0168df325485317e5
hugodose <hugo.dose@gmail.com>
2015-03-16 18:24:50 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -4,8 +4,11 @@
   <?php
   require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
  
-  //$retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
-  $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='1560115454240194', LNAME='e9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
+ 
+  echo $_POST['name'];
+  echo $_POST['content'];
+  //funciona: $retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
+  //funciona: $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
   echo '<br>'; 
   $retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('oi','oi2')" );
   echo '<br>';
@@ -17,8 +20,10 @@
 
   ?>
 
+
+
   <h2>Input</h2>
-  <form action="/sign" method="post">
+  <form action="/" method="post">
     <div><textarea name="name" rows="3" cols="60"></textarea></div>
     <div><textarea name="content" rows="3" cols="60"></textarea></div>
     <div><input type="submit" value="Input"></div>
