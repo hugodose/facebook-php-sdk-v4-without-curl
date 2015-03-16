8146c8acf6e6064d2718724be8b6bbb79269b225
hugodose <hugo.dose@gmail.com>
2015-03-16 18:59:26 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -6,17 +6,18 @@
  
   $varum = $_POST['name'];
   $vardois = $_POST['content'];
+  $sqltodo = $_POST['sql'];
   
   //$sqltodo = "INSERT INTO person (FNAME, LNAME) VALUES ('um','dois')";
-  $sqltodo = $_POST['name'];;
+  //$sqltodo = "DELETE from person WHERE ID > 4";
+  
   
   echo $sqltodo;
-  //funciona: $retorno = (new minhaclasse())->selectDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
+  //funciona: $retorno = (new minhaclasse())->usaDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
   //funciona: $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
   echo '<br>'; 
-  //funciona: $retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('$varum','$vardois')" );
+  $retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('$varum','$vardois')" );
   echo '<br>';
-  
   $retorno = (new minhaclasse())->usaDB("$sqltodo");
   echo '<br>';
  
@@ -31,8 +32,9 @@
 
   <h2>Input</h2>
   <form action="/gerencia.php" method="post">
-    <div><textarea name="name" rows="3" cols="60"></textarea></div>
-    <div><textarea name="content" rows="3" cols="60"></textarea></div>
+    <div><textarea name="name" rows="1" cols="20"></textarea></div>
+    <div><textarea name="content" rows="1" cols="20"></textarea></div>
+    <div><textarea name="sql" rows="1" cols="20"></textarea></div>
     <div><input type="submit" value="Input"></div>
   </form>
   </body>
