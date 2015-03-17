c1c64f16b0decc3964e10e05e93d118fedd3e6fc
hugodose <hugo.dose@gmail.com>
2015-03-17 13:35:20 +0000

    Update data.php

--- data.php
+++ data.php
@@ -2,4 +2,8 @@
   date_default_timezone_set('Europe/London');
   $date = date('d-m-Y H:i:s');
   echo $date;
+  
+  $my_datetime='2013-10-23 15:47:10';
+  echo date('d-m-Y H:i:s',strtotime("$my_datetime BRT"));
+
 ?>
