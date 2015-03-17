f4c6641ee42032872eee0f45dc0cbd2128fbc14a
hugodose <hugo.dose@gmail.com>
2015-03-17 17:21:30 +0000

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -16,18 +16,18 @@
   //funciona: $retorno = (new minhaclasse())->usaDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
   //funciona: $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
   echo '<br>'; 
-  $retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('$varum','$vardois')" );
+  //$retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('$varum','$vardois')" );
   echo '<br>';
   $retorno = (new minhaclasse())->usaDB("$sqltodo");
   foreach($retorno as $row) {
-  echo "<div>" . $row[0] . " " . $row[1] . " " . $row[2] . "</div>";
+  echo "<div>" . $row[0] . " | " . $row[1] . " | " . $row[2] . " | " . $row[3] . " | " . $row[4] . " | " . $row[5] . " | " . $row[6] . " | " . $row[7] . " | " . $row[8] . "</div>";
   }
   echo '<br>';
  
-  $retorno = (new minhaclasse())->usaDB('SELECT * from person');
-  foreach($retorno as $row) {
-  echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . " " . $row['Id'] . "</div>";
-  }
+  //$retorno = (new minhaclasse())->usaDB('SELECT * from person');
+  //foreach($retorno as $row) {
+  //echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . " " . $row['Id'] . "</div>";
+  //}
 
   ?>
 
