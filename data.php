08ae238e7149ccd597471d5bb04defe95b9b191f
hugodose <hugo.dose@gmail.com>
2015-03-17 13:49:06 +0000

    Update data.php

--- data.php
+++ data.php
@@ -4,9 +4,9 @@
   echo $date .' - Londres<br>';
   
   $my_datetime='2013-10-23 15:47:10';
-  echo date('d-m-Y H:i:s',strtotime("$my_datetime")) .' - BRT to Londres<br>';;
+  echo date('d-m-Y H:i:s',strtotime("$my_datetime BRT")) .' - BRT to Londres<br>';;
   
   date_default_timezone_set('Brazil/East');
-  echo date('d-m-Y H:i:s',strtotime("$date")) .' - Londres to BRT<br>';;
+  echo date('d-m-Y H:i:s',strtotime("$date UCT")) .' - Londres to BRT<br>';;
 
 ?>
