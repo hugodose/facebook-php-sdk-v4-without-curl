66c9f5d0b60501db85136de3a06832ff56bc0857
hugodose <hugo.dose@gmail.com>
2015-03-17 13:40:50 +0000

    Update data.php

--- data.php
+++ data.php
@@ -1,9 +1,12 @@
 <?php
   date_default_timezone_set('Europe/London');
   $date = date('d-m-Y H:i:s');
-  echo $date;
+  echo $date .' - Londres<br>';
   
   $my_datetime='2013-10-23 15:47:10';
-  echo date('d-m-Y H:i:s BRT',strtotime("$my_datetime BRT"));
+  echo date('d-m-Y H:i:s',strtotime("$my_datetime")) .' - BRT to Londres<br>';;
+  
+  date_default_timezone_set('Brazil/East');
+  echo date('d-m-Y H:i:s',strtotime("$date")) .' - Londres to BRT<br>';;
 
 ?>
