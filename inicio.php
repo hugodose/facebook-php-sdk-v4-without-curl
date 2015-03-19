cd3204e912f74a4e4ea6f65d3533c563948df0b9
hugodose <hugo.dose@gmail.com>
2015-03-19 18:50:56 +0000

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -98,6 +98,14 @@ if ( isset( $session ) ) {
   } //iterate through friends graph
   
   
+   $pagefeed = (new FacebookRequest( $session, 'GET', '/ellytran.cadie/feed' ))->execute()->getGraphObject()->asArray();
+   echo '<pre>' . print_r( $pagefeed, 1 ) . '</pre>';
+   foreach ($pagefeed['data'] as $key => $value) {
+   echo '<img class="friendthumb" src = "',$value->link,'"/>';
+   echo " - ", $value->message,'<br>';
+   } //iterate through friends graph
+  
+  
   
 } else {
     echo '<br> $session NAO existe <br>';
