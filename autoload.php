a81d361928c2c241e160ece178c2e7f1ab23cdf1
hugodose <hugo.dose@gmail.com>
2015-03-19 18:57:17 +0000

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -5,9 +5,13 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRedirectLoginHelpe
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequest.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookResponse.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookSDKException.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookServerException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookRequestException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookAuthorizationException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/GraphObject.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/FacebookPermissionException.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/src/Facebook/GraphUser.php');
+
+
+
 ?>
