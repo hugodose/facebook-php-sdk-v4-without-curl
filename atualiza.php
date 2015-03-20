e00d55bf15f61ad2bae885aec17a6a9cdcead82b
hugodose <hugo.dose@gmail.com>
2015-03-20 13:07:24 +0000

    Update atualiza.php

--- atualiza.php
+++ atualiza.php
@@ -1,54 +1,19 @@
 <html>
  <body>
   <h2>Resultados</h2>
-  <form action="atualiza.php" method="get">
-    <input type="button" name="B1" value="B1">
-    <input type="button" name="B2" value="B2">
-    <input type="button" name="insert" value="insert">
-    <input type="button" name="select" value="select">
-    <input type="Submit" name="Submit1"/>
-
-    <!-- <a href="http://www.google.com?act=google">Google</a>
-    <a href="http://www.yahoo.com?act=yahoo">yahoo</a>
-     -->
+  <form action="/atualiza.php" method="POST">
+  <div><textarea name="Time1" rows="1" cols="20"></textarea></div>
+  <div><textarea name="Time2" rows="1" cols="20"></textarea></div>
+  <div><textarea name="DataJogo" rows="1" cols="20"></textarea></div>
+  <input type="Submit" name="Input" value="Clique"/>
   </form>
 
   
  <?php
  require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
 
-if(isset($_GET["Submit1"])){   
-        echo "Submit1";
-}
-if(isset($_GET["B1"])){
-        echo "B1";
-} 
-if(isset($_GET["B2"])){
-        echo "B2"; 
-}
 
 
-
-
-
- if($_GET){
-    if(isset($_GET['insert'])){
-        insert();
-    }elseif(isset($_GET['select'])){
-        select();
-    }
- }
-
-    function select()
-    {
-       echo "The select function is called.";
-    }
-    function insert()
-    {
-       echo "The insert function is called.";
-    }
-
- 
  
  
  
@@ -68,11 +33,11 @@ if(isset($_GET["B2"])){
  echo '<br>';
  
 
- if (isset($_POST['submit2'])) {
+ if (isset($_POST['Input'])) {
  
  echo $_POST['Time1'];
  echo $_POST['Time2'];
- echo $_POST['Data'];
+ echo $_POST['DataJogo'];
   
  }
   
