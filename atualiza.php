2fc32907419ee4be310f0dc4dee0ffc8bfb19f20
hugodose <hugo.dose@gmail.com>
2015-03-20 11:42:09 +0000

    Update atualiza.php

--- atualiza.php
+++ atualiza.php
@@ -8,11 +8,11 @@
  $sql = "SELECT Time1, Time2, DataJogo from Jogos WHERE Resultado = ''";
  $retorno = (new minhaclasse())->usaDB("$sql");
  foreach($retorno as $row) {
- echo "<form action="" method="POST">";
- echo "<div><textarea name="Time1" rows="1" cols="20">" . $row[0] . "</textarea></div>";
- echo "<div><textarea name="Time2" rows="1" cols="20">" . $row[1] . "</textarea></div>";
- echo "<div><textarea name="DataJogo" rows="1" cols="20">" . $row[2] . "</textarea></div>";
- echo "<div><input type="submit" value="submit"></div>";
+ echo "<form action='' method='POST'>";
+ echo "<div><textarea name='Time1' rows='1' cols='20'>" . $row[0] . "</textarea></div>";
+ echo "<div><textarea name='Time2' rows='1' cols='20'>" . $row[1] . "</textarea></div>";
+ echo "<div><textarea name='DataJogo' rows='1' cols='20'>" . $row[2] . "</textarea></div>";
+ echo "<div><input type='submit' value='submit'></div>";
  echo "</form>";
  }
  echo '<br>';
