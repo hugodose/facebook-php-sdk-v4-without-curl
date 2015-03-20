d8d0b670e2bb0b36cdc4d8980f62c4fa618265af
hugodose <hugo.dose@gmail.com>
2015-03-20 11:40:24 +0000

    Update atualiza.php

--- atualiza.php
+++ atualiza.php
@@ -5,7 +5,7 @@
  <?php
  require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
 
- $sql = 'SELECT Time1, Time2, DataJogo from Jogos WHERE Resultado = '''
+ $sql = "SELECT Time1, Time2, DataJogo from Jogos WHERE Resultado = ''";
  $retorno = (new minhaclasse())->usaDB("$sql");
  foreach($retorno as $row) {
  echo "<form action="" method="POST">";
