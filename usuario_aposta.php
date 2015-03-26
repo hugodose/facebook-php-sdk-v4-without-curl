96b4d56579fc948dc24191e3bdf18fdb3afe6c90
hugodose <hugo.dose@gmail.com>
2015-03-26 12:19:40 +0000

    Update usuario_aposta.php

--- usuario_aposta.php
+++ usuario_aposta.php
@@ -53,7 +53,7 @@
    
    $sql = "SELECT $Escolha from Jogos WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
    $retorno = (new minhaclasse())->usaDB("$sql");
-   var_dump $retorno; 
+   var_dump($retorno); 
  
    
    
