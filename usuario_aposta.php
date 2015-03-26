f8210813d7dbf0d05e82eee729654e3aa53e2fca
hugodose <hugo.dose@gmail.com>
2015-03-26 12:09:09 +0000

    Update usuario_aposta.php

--- usuario_aposta.php
+++ usuario_aposta.php
@@ -16,15 +16,21 @@
 
  if (isset($_POST['Load'])) {
    //$sql = "SELECT Time1, Time2, DataJogo from Jogos WHERE Resultado = ''";
-   $sql = "SELECT Time1, Time2, DataJogo, Resultado from Jogos";
+   $sql = "SELECT Campeonato, Time1, Time2, DataJogo, Casa, Empate, Fora, Resultado from Jogos";
    $retorno = (new minhaclasse())->usaDB("$sql");
    foreach($retorno as $row) {
      echo "<form action='' method='POST'>";
      echo "<div>"; 
-     echo "<textarea name='Time1' rows='1' cols='20'>" . $row[0] . "</textarea>";
-     echo "<textarea name='Time2' rows='1' cols='20'>" . $row[1] . "</textarea>";
-     echo "<textarea name='DataJogo' rows='1' cols='20'>" . $row[2] . "</textarea>";
-     echo "<textarea name='Resultado' rows='1' cols='20'>" . $row[3] . "</textarea>";
+     echo "<textarea name='Campeonato' rows='1' cols='20'>" . $row[0] . "</textarea>";
+     echo "<textarea name='Time1' rows='1' cols='20'>" . $row[1] . "</textarea>";
+     echo "<textarea name='Time2' rows='1' cols='20'>" . $row[2] . "</textarea>";
+     echo "<textarea name='DataJogo' rows='1' cols='20'>" . $row[3] . "</textarea>";
+     echo "<textarea name='Casa' rows='1' cols='20'>" . $row[4] . "</textarea>";
+     echo "<textarea name='Empate' rows='1' cols='20'>" . $row[5] . "</textarea>";
+     echo "<textarea name='Fora' rows='1' cols='20'>" . $row[6] . "</textarea>";
+     echo "<textarea name='Resultado' rows='1' cols='20'>" . $row[7] . "</textarea>";
+     echo "<textarea name='Notional' rows='1' cols='20'>"..notional.."</textarea>";
+     echo "<textarea name='Escolha' rows='1' cols='20'>"..escolha.."</textarea>";
      echo "<input type='submit' name='Input' value='Envia'>";
      echo "</div>"; 
      echo "</form>";
@@ -35,27 +41,20 @@
 
  if (isset($_POST['Input'])) {
  
+   $Campeonato = $_POST['Campeonato'];
    $Time1 = $_POST['Time1'];
    $Time2 = $_POST['Time2'];
    $DataJogo =  $_POST['DataJogo'];
-   $Resultado =  $_POST['Resultado'];
+   $Casa=  $_POST['Casa'];
+   $Empate =  $_POST['Empate'];
+   $Fora =  $_POST['Fora'];
+   $Notional =  $_POST['Notional'];
+   $Escolha =  $_POST['Escolha'];
    
+   $sql = "SELECT $Escolha from Jogos WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
+   $retorno = (new minhaclasse())->usaDB("$sql");
+   var_dump $retorno; 
  
-   $sql = "UPDATE Jogos SET Resultado='$Resultado' WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
-   $retorno = (new minhaclasse())->usaDB("$sql");
-   
-   $sql = "UPDATE Apostas SET Resultado='$Resultado' WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
-   $retorno = (new minhaclasse())->usaDB("$sql");
-   
-   $sql = "UPDATE Apostas SET Pnl=(Notional * (Odds-1)) WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo' AND Escolha=Resultado";
-   $retorno = (new minhaclasse())->usaDB("$sql");
-   $sql = "UPDATE Apostas SET Pnl=0 WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo' AND Escolha<>Resultado";
-   $retorno = (new minhaclasse())->usaDB("$sql");
-   $sql = "SELECT userid, sum(pnl) from Apostas GROUP BY userid";
-   $retorno = (new minhaclasse())->usaDB("$sql");
-   foreach($retorno as $row) {
-     echo "<div>" . $row[0] . " | " . $row[1] . " | " . $row[2] . " | " . $row[3] . " | " . $row[4] . " | " . $row[5] . " | " . $row[6] . " | " . $row[7] . " | " . $row[8] . " | " . $row[9] . " | " . $row[10] . " | " . $row[11] . " | " . $row[12] . "</div>";
-   }
    
    
  }
