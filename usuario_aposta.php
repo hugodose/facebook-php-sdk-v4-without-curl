7ac08d69c377faf9aebe6cdb522c3a010c9e7076
hugodose <hugo.dose@gmail.com>
2015-03-26 13:41:02 +0000

    Update usuario_aposta.php

--- usuario_aposta.php
+++ usuario_aposta.php
@@ -74,8 +74,7 @@
       $retorno = (new minhaclasse())->usaDB("$sql");
       foreach($retorno as $row) {
         echo "<div>" . $row[0] . " | " . $row[1] . " | " . $row[2] . " | " . $row[3] . " | " . $row[4] . " | " . $row[5] . " | " . $row[6] . " | " . $row[7] . " | " . $row[8] . " | " . $row[9] . " | " . $row[10] . " | " . $row[11] . " | " . $row[12] . "</div>";
-    }
-    else{
+    } else {
       echo "Odds invalido: " . $odds . '<>' . $row[$Escolha];
     }  
    }
