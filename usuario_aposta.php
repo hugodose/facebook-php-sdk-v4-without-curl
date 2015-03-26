09f3574966a988170e4d2df369989d87b82e8263
hugodose <hugo.dose@gmail.com>
2015-03-26 12:35:58 +0000

    Update usuario_aposta.php

--- usuario_aposta.php
+++ usuario_aposta.php
@@ -53,7 +53,7 @@
    
    $sql = "SELECT $Escolha from Jogos WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
    $retorno = (new minhaclasse())->usaDB("$sql");
-   $odds = $this->$retorno;
+   $odds = $obj->$retorno;
    echo $odds[0];
    
    
