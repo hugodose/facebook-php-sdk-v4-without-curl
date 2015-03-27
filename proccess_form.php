6c3882df80195085078c889bac980b80cad1f5dd
hugodose <hugo.dose@gmail.com>
2015-03-27 15:31:02 +0000

    Update proccess_form.php

--- proccess_form.php
+++ proccess_form.php
@@ -1,7 +1,6 @@
-<?php
-    $date1 = GET_['start_date'];
-    $date2 = GET_['end_date'];
 
-    echo 'Variavel1: ' . $date1;
-    echo 'Variavel2: ' . $date2;
+<?php
+echo "you entered firstname=".$_POST["firstname"]."<br>";
+echo "PHP is sending this back and it hits your javascript function updatepage(str).    <br>Load this up with your xml or json data and parse it.<br> Or present a table from php   with innerHtml";
+echo "<table border='1'><tr><td>Row 1 Cell 1</td><td>Row 1 Cell 2</td></tr><tr><td>Row 2 Cell 1</td><td>Row 2 Cell 2</td></tr></table>";
 ?>
