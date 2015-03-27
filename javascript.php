7c933e7262f9e7f96cd397286475db71de2e9863
hugodose <hugo.dose@gmail.com>
2015-03-27 13:53:31 +0000

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,5 +1,17 @@
 <input type="button" id="meubotao" value="Clique aqui" onclick="Consulta();" />
 <input type="text" id="meutexto" value="..." />
+
+echo "<form action='zzzzz.php' method='POST'>";
+echo "<div>";
+echo "<textarea name='Time1' rows='1' cols='20'>" . $row[0] . "</textarea>";
+echo "<textarea name='Time2' rows='1' cols='20'>" . $row[1] . "</textarea>";
+echo "<textarea name='DataJogo' rows='1' cols='20'>" . $row[2] . "</textarea>";
+echo "<textarea name='Resultado' rows='1' cols='20'>" . $row[3] . "</textarea>";
+echo "<input type='submit' name='Input' value='Envia'>";
+echo "</div>";
+echo "</form>";
+
+
 <script>
     //function reqListener () {
     //  console.log(this.responseText);
@@ -9,7 +21,7 @@ function Consulta () {
     oReq.onload = function() {
         //This is where you handle what to do with the response.
         //The actual data is found on this.responseText
-        document.getElementById('meutexto').value = this.responseText;
+        document.getElementById('meutexto').value = this.responseText[0];
         //alert(this.responseText); 
     };
     oReq.open("get", "get-data.php", true);
