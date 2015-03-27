f1f76c2ade523a66fa3d3e9f56e83e614610a57c
hugodose <hugo.dose@gmail.com>
2015-03-27 14:01:55 +0000

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -1,4 +1,19 @@
 <?php
 $minhamatriz = array('oi1', 'oi2');
 echo json_encode($minhamatriz);
+
+echo "<table border='1'>
+<tr>
+<th>Heading1</th>
+<th>Heading2</th>
+</tr>";
+
+  echo "<tr>";
+  echo "<td>" . $minhamatriz[0] . "</td>";
+  echo "<td>" . $minhamatriz[1] . "</td>";
+  echo "</tr>";
+echo "</table>";
+
+
+
 ?>
