46e0f90d9ceabf1ee7aba6350d38e1a1f474c4b4
hugodose <hugo.dose@gmail.com>
2015-03-27 13:10:04 +0000

    Create javascript.php

---
+++ javascript.php
@@ -0,0 +1,18 @@
+<input type="button" id="meubotao" value="Clique aqui" onclick="Consulta();" />
+<script>
+    function reqListener () {
+      console.log(this.responseText);
+    }
+
+    var oReq = new XMLHttpRequest(); //New request object
+    oReq.onload = function() {
+        //This is where you handle what to do with the response.
+        //The actual data is found on this.responseText
+        alert(this.responseText); 
+    };
+    oReq.open("get", "get-data.php", true);
+    //                               ^ Don't block the rest of the execution.
+    //                                 Don't wait until the request finishes to 
+    //                                 continue.
+    oReq.send();
+</script>
