6fd0c5dc819eebb681e2e1108a0b72e303d812b7
hugodose <hugo.dose@gmail.com>
2015-03-27 14:56:24 +0000

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -14,20 +14,18 @@ function Consulta () {
     oReq.onload = function() {
         //This is where you handle what to do with the response.
         //The actual data is found on this.responseText
-        //document.getElementById('meutexto').value = this.responseText[0];
-        document.getElementById('tabelaphp1').innerHTML=this.responseText;
-        document.getElementById('tabelaphp2').innerHTML=oReq.responseText;
+        document.getElementById('meutexto').value = this.responseText;  //retorna variavel
+        document.getElementById('tabelaphp1').innerHTML=this.responseText; //retorna html
+        document.getElementById('tabelaphp2').innerHTML=oReq.responseText; //retorna html
         //alert(this.responseText); 
     };
-    oReq.onreadystatechange=function(){
-        if (oReq.readyState==4 && oReq.status==200) //ready
-        {
-            document.getElementById('tabelaphp3').innerHTML=this.responseText;
-            document.getElementById('tabelaphp4').innerHTML=oReq.responseText;
-            
-
-        }
-    }
+    //oReq.onreadystatechange=function(){
+    //    if (oReq.readyState==4 && oReq.status==200) //ready
+    //    {
+    //        document.getElementById('tabelaphp3').innerHTML=this.responseText;
+    //        document.getElementById('tabelaphp4').innerHTML=oReq.responseText;
+    //    }
+    //}
     oReq.open("get", "get-data.php", true);
     //                               ^ Don't block the rest of the execution.
     //                                 Don't wait until the request finishes to 
