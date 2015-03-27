7695df1fec9542c4478f1d1e918b3725aa9b6ae7
hugodose <hugo.dose@gmail.com>
2015-03-27 13:14:50 +0000

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,13 +1,15 @@
 <input type="button" id="meubotao" value="Clique aqui" onclick="Consulta();" />
+<input type="text" id="meutexto" value="..." />
 <script>
-    function reqListener () {
-      console.log(this.responseText);
-    }
-
+    //function reqListener () {
+    //  console.log(this.responseText);
+    //}
+function Consulta () {
     var oReq = new XMLHttpRequest(); //New request object
     oReq.onload = function() {
         //This is where you handle what to do with the response.
         //The actual data is found on this.responseText
+        document.getElementById('meutexto').value = this.responseText;
         alert(this.responseText); 
     };
     oReq.open("get", "get-data.php", true);
@@ -15,4 +17,5 @@
     //                                 Don't wait until the request finishes to 
     //                                 continue.
     oReq.send();
+}
 </script>
