14e47e9899940d6a170a6712caa3ba180601aefe
hugodose <hugo.dose@gmail.com>
2015-03-31 20:03:20 +0100

    Update div_minhasapostas.php

--- div_minhasapostas.php
+++ div_minhasapostas.php
@@ -1,21 +1,25 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
-
-if (isset($_POST['Load'])) {
-  //$sql = "SELECT Time1, Time2, DataJogo from Jogos WHERE Resultado = ''";
-  $sql = "SELECT Time1, Time2, DataJogo, Resultado from Jogos";
+  $FBid = '12001';
+  $sql = "SELECT id, data, campeonato, time1, time2, datajogo, notional, escolha, odds, resultado, pnl from Apostas WHERE id = '$FBid'";
   $retorno = (new minhaclasse())->usaDB("$sql");
   foreach($retorno as $row) {
-    echo "<form action='' method='POST'>";
-    echo "<div>";
-    echo "<textarea name='Time1' rows='1' cols='20'>" . $row[0] . "</textarea>";
-    echo "<textarea name='Time2' rows='1' cols='20'>" . $row[1] . "</textarea>";
-    echo "<textarea name='DataJogo' rows='1' cols='20'>" . $row[2] . "</textarea>";
-    echo "<textarea name='Resultado' rows='1' cols='20'>" . $row[3] . "</textarea>";
-    echo "<input type='submit' name='Input' value='Envia'>";
-    echo "</div>";
-    echo "</form>";
+     $formname = 'Aposta' . $row[0]; 
+     echo "<form name='" . $formname . "'>";
+     echo "<div>"; 
+     echo "<textarea name='Data' rows='1' cols='20'>" . $row[1] . "</textarea>";
+     echo "<textarea name='Campeonato' rows='1' cols='20'>" . $row[2] . "</textarea>";
+     echo "<textarea name='Time1' rows='1' cols='20'>" . $row[3] . "</textarea>";
+     echo "<textarea name='Time2' rows='1' cols='20'>" . $row[4] . "</textarea>";
+     echo "<textarea name='DataJogo' rows='1' cols='20'>" . $row[5] . "</textarea>";
+     echo "<textarea name='Notional' rows='1' cols='20'>" . $row[6] . "</textarea>";
+     echo "<textarea name='Escolha' rows='1' cols='20'>" . $row[7] . "</textarea>";
+     echo "<textarea name='Odds' rows='1' cols='20'>" . $row[8] . "</textarea>";
+     echo "<textarea name='Resultado' rows='1' cols='20'>" . $row[9] . "</textarea>";
+     echo "<textarea name='Pnl' rows='1' cols='20'>" . $row[10] . "</textarea>";
+     echo "</div>"; 
+     echo "</form>";
   }
 echo '<br>';
-}
+
 ?>
