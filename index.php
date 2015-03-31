5b1d76e847ce0ff60fce030bab1cdb683005e1b2
hugodose <hugo.dose@gmail.com>
2015-03-31 19:33:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -113,7 +113,8 @@ echo 'Aposta Gol é um aplicativo gratuito';
    $sql = "SELECT Id, Campeonato, Time1, Time2, DataJogo, Casa, Empate, Fora, Resultado from Jogos";
    $retorno = (new minhaclasse())->usaDB("$sql");
    foreach($retorno as $row) {
-     echo "<form name='" . $row[0] . "'>";
+     $formname = 'Jogo' . $row[0]; 
+     echo "<form name='" . $formname . "'>";
      echo "<div>"; 
      echo "<textarea name='Campeonato' rows='1' cols='20'>" . $row[1] . "</textarea>";
      echo "<textarea name='Time1' rows='1' cols='20'>" . $row[2] . "</textarea>";
@@ -125,7 +126,7 @@ echo 'Aposta Gol é um aplicativo gratuito';
      echo "<textarea name='Resultado' rows='1' cols='20'>" . $row[8] . "</textarea>";
      echo "<textarea name='Notional' rows='1' cols='20'> ..notional.. </textarea>";
      echo "<textarea name='Escolha' rows='1' cols='20'> ..escolha.. </textarea>";
-     echo '<input value="Go" type="button" onclick="JavaScript:xmlhttpPost(\'div_enviaapostas.php\',\'' . $row[0] . '\')">';
+     echo '<input value="Go" type="button" onclick="JavaScript:xmlhttpPost(\'div_enviaapostas.php\',\'' . $formname . '\')">';
      echo "</div>"; 
      echo "</form>";
      }
