2c106bb6800c9d91c0d1fe9f8f63a25593634ffb
hugodose <hugo.dose@gmail.com>
2015-09-03 18:32:26 +0100

    Update div_enviaapostas.php

--- div_enviaapostas.php
+++ div_enviaapostas.php
@@ -17,8 +17,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
    $userid = $_SESSION['FBid'];
    echo $Escolha . '<br>';
    
-   //Valida Horario e Odds na tablea Jogos
-   $sql = "SELECT $Escolha from Jogos WHERE Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
+   //Valida Horario e Odds na tabela Jogos
+   $sql = "SELECT $Escolha from Jogos WHERE Campeonato='$Campeonato' AND Time1='$Time1' AND Time2='$Time2' AND DataJogo='$DataJogo'";
    $retorno = (new minhaclasse())->usaDB("$sql");
    foreach($retorno as $row) {
       $odds = $row[$Escolha];
@@ -29,7 +29,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
    echo $date . '<br>';
    
    if ($date < $_POST['DataJogo']){
-     echo 'Hora Atual: ' . $date . ' | Hora do Jogo: ' . $DataJogo . ' | Aposta Aberta <br>' ;
+     echo 'Hora Atual: ' . $date . ' | Hora do Jogo: ' . $DataJogo . ' | Janela de Apostas Aberta <br>' ;
 
      if ($odds == $_POST[$Escolha]){
         echo $odds . ' = ' . $_POST[$Escolha] . '<br>';
@@ -38,16 +38,16 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
         $sql = "INSERT INTO Apostas (data, userid, campeonato, time1, time2, datajogo, notional, escolha, odds) VALUES ('$date', '$userid', '$Campeonato', '$Time1', '$Time2', '$DataJogo', $Notional, '$Escolha', $odds)";
         $retorno = (new minhaclasse())->usaDB("$sql");
 
-        $sql = "SELECT * from Apostas WHERE userid = $userid";
-        $retorno = (new minhaclasse())->usaDB("$sql");
-        foreach($retorno as $row) {
-          echo "<div>" . $row[0] . " | " . $row[1] . " | " . $row[2] . " | " . $row[3] . " | " . $row[4] . " | " . $row[5] . " | " . $row[6] . " | " . $row[7] . " | " . $row[8] . " | " . $row[9] . " | " . $row[10] . " | " . $row[11] . " | " . $row[12] . "</div>";
-        }
+        //$sql = "SELECT * from Apostas WHERE userid = $userid";
+        //$retorno = (new minhaclasse())->usaDB("$sql");
+        //foreach($retorno as $row) {
+        //    echo "<div>" . $row[0] . " | " . $row[1] . " | " . $row[2] . " | " . $row[3] . " | " . $row[4] . " | " . $row[5] . " | " . $row[6] . " | " . $row[7] . " | " . $row[8] . " | " . $row[9] . " | " . $row[10] . " | " . $row[11] . " | " . $row[12] . "</div>";
+        //}
      } else {
         echo "Odds invalido: " . $odds . '<>' . $row[$Escolha];
      }  
    } else {
-     echo 'Hora Atual: ' . $date . ' | Hora do Jogo: ' . $DataJogo . ' | Aposta Fechada <br>' ;
+     echo 'Hora Atual: ' . $date . ' | Hora do Jogo: ' . $DataJogo . ' | Janela de Apostas Fechada <br>' ;
    }
 
 ?>
