cf97576d403eb56f9fbd9699aedb461e7e147aae
hugodose <hugo.dose@gmail.com>
2015-09-03 10:15:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ FacebookSession::setDefaultApplication($appID,$appSecret);
 //
 $helper1 = new FacebookSession();
 $accessToken = $helper1->getToken();
-echo '<h3>Signed Request</h3>';
-var_dump($helper1->getSignedRequest());
+//echo '<h3>Signed Request</h3>';
+//var_dump($helper1->getSignedRequest());
 echo '<h3>Access Token</h3>';
 var_dump($accessToken->getValue());
 //
