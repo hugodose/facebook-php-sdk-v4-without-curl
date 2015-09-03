5571c9ccafa45b86cedbb2e631be8ed55d32cb7d
hugodose <hugo.dose@gmail.com>
2015-09-03 16:45:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -145,8 +145,9 @@ if ( isset( $session ) ) {
               updatepage2(self.xmlHttpReq.responseText);
         }
       }
-      input_FBid = document.getElementById("FBid").innerHTML;
-      self.xmlHttpReq.send('FBid='+input_FBid);
+      //input_FBid = document.getElementById("FBid").innerHTML;
+      //self.xmlHttpReq.send('FBid='+input_FBid);
+      self.xmlHttpReq.send();
   }
 
   function updatepage2(str){
@@ -181,7 +182,8 @@ if ( isset( $session ) ) {
 
   function getstring(formID) {
      var form = document.forms[formID];
-          squery = 'FBid=' + input_FBid + '&Campeonato=' + escape(form.Campeonato.value) + '&Time1=' + escape(form.Time1.value) + '&Time2=' + escape(form.Time2.value) + '&DataJogo=' + escape(form.DataJogo.value) + '&Casa=' + escape(form.Casa.value) + '&Empate=' + escape(form.Empate.value) + '&Fora=' + escape(form.Fora.value) + '&Notional=' + escape(form.Notional.value) + '&Escolha=' + escape(form.Escolha.value);
+          //squery = 'FBid=' + input_FBid + '&Campeonato=' + escape(form.Campeonato.value) + '&Time1=' + escape(form.Time1.value) + '&Time2=' + escape(form.Time2.value) + '&DataJogo=' + escape(form.DataJogo.value) + '&Casa=' + escape(form.Casa.value) + '&Empate=' + escape(form.Empate.value) + '&Fora=' + escape(form.Fora.value) + '&Notional=' + escape(form.Notional.value) + '&Escolha=' + escape(form.Escolha.value);
+          squery = 'Campeonato=' + escape(form.Campeonato.value) + '&Time1=' + escape(form.Time1.value) + '&Time2=' + escape(form.Time2.value) + '&DataJogo=' + escape(form.DataJogo.value) + '&Casa=' + escape(form.Casa.value) + '&Empate=' + escape(form.Empate.value) + '&Fora=' + escape(form.Fora.value) + '&Notional=' + escape(form.Notional.value) + '&Escolha=' + escape(form.Escolha.value);
           return squery;
      }
 
