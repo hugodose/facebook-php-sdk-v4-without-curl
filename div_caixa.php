70315cfcc8e908cefbfa2b77d4d35130ef6ca5ca
hugodose <hugo.dose@gmail.com>
2015-09-04 16:02:09 +0100

    Update div_caixa.php

--- div_caixa.php
+++ div_caixa.php
@@ -8,15 +8,15 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
   $sql = "SELECT caixa, margem, pnl, risco from Clientes WHERE userid = '$FBid'";
   $retorno = (new minhaclasse())->usaDB("$sql");
   foreach($retorno as $row) {
-     $formname = 'Caixa' . $row[0]; 
+     $formname = 'Caixa'; 
      echo "<form name='" . $formname . "'>";
      echo "<div>"; 
-     echo "<input type='text' size='20' name='Ativos' value='" . $row[1] + $row[2] . "'></input>";
-     echo "<input type='text' size='20' name='Caixa' value='" . $row[1] . "'></input>";
-     echo "<input type='text' size='20' name='Margem' value='" . $row[2] . "'></input>";
-     echo "<input type='text' size='18' name='Retorno' value='" . $row[3] . "'></input>";
-     echo "<input type='text' size='18' name='Risco' value='" . $row[4] . "'></input>";
-     echo "<input type='text' size='20' name='Retorno/Risco' value='" . $row[3]/$row[4]*100 . "%'></input>";
+     echo "<input type='text' size='20' name='Ativos' value='" . $row[0] + $row[1] . "'></input>";
+     echo "<input type='text' size='20' name='Caixa' value='" . $row[0] . "'></input>";
+     echo "<input type='text' size='20' name='Margem' value='" . $row[1] . "'></input>";
+     echo "<input type='text' size='18' name='Retorno' value='" . $row[2] . "'></input>";
+     echo "<input type='text' size='18' name='Risco' value='" . $row[3] . "'></input>";
+     echo "<input type='text' size='20' name='Retorno/Risco' value='" . $row[2]/$row[3]*100 . "%'></input>";
      echo "</div>"; 
      echo "</form>";
   }
