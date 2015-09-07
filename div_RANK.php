a278833e200bcd260d80ef1b88c6e3f919cef805
hugodose <hugo.dose@gmail.com>
2015-09-07 17:13:15 +0100

    Update div_RANK.php

--- div_RANK.php
+++ div_RANK.php
@@ -6,18 +6,19 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
   $userid = $_SESSION['FBid'];
 
   
-  $sql = "SELECT userid, Nome, SUM(caixa + margem) AS Ativos FROM Clientes GROUP BY userid ORDER BY Ativos desc";
+  $sql = "SELECT userid, Nome, SUM(caixa + margem) AS Ativos, Ativos/Risco *100 FROM Clientes GROUP BY userid ORDER BY Ativos desc";
   $retorno = (new minhaclasse())->usaDB("$sql");
   $formname = 'Rank'; 
   echo "<form name='" . $formname . "'>";
   $i = 1;
   foreach($retorno as $row) {
      echo "<div>"; 
-     echo "<input type='text' size='20' name='Ranking' value='" . i . "'></input>";
-     $i++;
      echo "<img src='https://graph.facebook.com/".$row[0]."/picture?type=small'/>";
+     echo "<input type='text' size='4' name='Ranking' value='" . i . "'></input>";
+     $i++;
      echo "<input type='text' size='20' name='UserID' value='" . $row[1] . "'></input>";
-     echo "<input type='text' size='20' name='Ativos' value='" . $row[2] . "'></input>";
+     echo "<input type='text' size='6' name='Ativos' value='" . $row[2] . "'></input>";
+     echo "<input type='text' size='6' name='Ativos' value='" . $row[3] . "%'></input>";
      echo "</div>"; 
      echo "</form>";
   }
