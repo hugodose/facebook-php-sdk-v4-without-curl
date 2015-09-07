b108b001071ae51208243d9488ddef189e789444
hugodose <hugo.dose@gmail.com>
2015-09-07 15:58:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -234,7 +234,41 @@ if ( isset( $session ) ) {
       document.getElementById("sentback").innerHTML = str;
       xmlhttpPostCAIXA('div_caixa.php','caixa'); // reload caixa
   }
+</script>  
+
+<script language="Javascript">  
+var $rows = $('#table tr');
+$('#search').keyup(function() {
+    
+    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+    
+    $rows.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
+    }).hide();
+});  
 </script>
+
+
+<input type="text" id="search" placeholder="Type to search">
+<table id="table">
+   <tr>
+      <td>Apple</td>
+      <td>Green</td>
+   </tr>
+   <tr>
+      <td>Grapes</td>
+      <td>Green</td>
+   </tr>
+   <tr>
+      <td>Orange</td>
+      <td>Orange</td>
+   </tr>
+</table>
+
+
             
 <?php
    echo "<div id='sentback'></div>";
