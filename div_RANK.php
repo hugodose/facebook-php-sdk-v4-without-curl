0c24ba04a914afb731ba702f2e80682b51f4a1f9
hugodose <hugo.dose@gmail.com>
2015-09-07 16:49:53 +0100

    Update div_RANK.php

--- div_RANK.php
+++ div_RANK.php
@@ -6,14 +6,15 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
   $userid = $_SESSION['FBid'];
 
   
-  $sql = "SELECT userid, SUM(caixa + margem) AS Ativos FROM Clientes GROUP BY userid ORDER BY Ativos desc";
+  $sql = "SELECT userid, Nome, SUM(caixa + margem) AS Ativos FROM Clientes GROUP BY userid ORDER BY Ativos desc";
   $retorno = (new minhaclasse())->usaDB("$sql");
   foreach($retorno as $row) {
      $formname = 'Rank'; 
      echo "<form name='" . $formname . "'>";
      echo "<div>"; 
-     echo "<input type='text' size='20' name='UserID' value='" . $row[0] . "'></input>";
-     echo "<input type='text' size='20' name='Ativos' value='" . $row[1] . "'></input>";
+     echo "<img src='https://graph.facebook.com/".$row[0]."/picture?type=normal'/>";
+     echo "<input type='text' size='20' name='UserID' value='" . $row[1] . "'></input>";
+     echo "<input type='text' size='20' name='Ativos' value='" . $row[2] . "'></input>";
      echo "</div>"; 
      echo "</form>";
   }
